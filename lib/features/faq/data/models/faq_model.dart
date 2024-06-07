2ccf356eac99ddeb136ce50fe2b946d234dd7126
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2024-06-07 11:00:22 +0700

    fix: Static data faq items

--- lib/features/faq/data/models/faq_model.dart
+++ lib/features/faq/data/models/faq_model.dart
@@ -1,63 +1,24 @@
-import 'package:equatable/equatable.dart';
-
+import '../../domain/entities/faq_entity.dart';
 import 'faq_detail_model.dart';
 
-enum FaqType {
-  questionAnswer,
-  tutorials,
-}
-
-class FaqModel extends Equatable {
-  final String? title;
-  final String? imageUrl;
-  final FaqType? type;
-  final List<FaqDetailModel>? faqDetails;
-
+class FaqModel extends FaqEntity {
   const FaqModel({
-    this.title,
-    this.imageUrl,
-    this.type,
-    this.faqDetails,
+    super.title,
+    super.imageUrl,
+    super.type,
+    super.faqDetails,
   });
 
-  @override
-  List<Object?> get props => [
-        title,
-        imageUrl,
-        type,
-        faqDetails,
-      ];
+  factory FaqModel.fromJson(Map<String, dynamic> json) {
+    return FaqModel(
+      title: json['title'],
+      imageUrl: json['image_url'],
+      type: json['type'],
+      faqDetails: (json['faq_details'] is List)
+          ? (json['faq_details'] as List).map((e) {
+              return FaqDetailModel.fromJson(e);
+            }).toList()
+          : null,
+    );
+  }
 }
-
-final mockFaqs = [
-  mockFaqQuestionAnswers,
-  mockFaqTutorial,
-  mockFaqQuestionAnswers,
-  mockFaqTutorial,
-];
-
-const mockFaqQuestionAnswers = FaqModel(
-  title: "After Care",
-  type: FaqType.questionAnswer,
-  faqDetails: [
-    mockFaqDetail,
-    mockFaqDetail,
-    mockFaqDetail,
-  ],
-);
-
-const mockFaqTutorial = FaqModel(
-  title: "Tindakan Preventif Usai Injeksi LAMS",
-  type: FaqType.tutorials,
-  faqDetails: [
-    mockFaqDetail,
-    mockFaqDetail,
-    mockFaqDetail,
-  ],
-);
-
-const mockFaqDetail = FaqDetailModel(
-  title: "Kenapa ukuran dan bentuk badan tetap sama?",
-  description:
-      "Karena baru beberapa minggu pasca-perawatan, kemajuan mungkin tak terlihat signifikan. \nBahkan setelah sebulan, ada yang belum melihat perubahan. \n\nPerlu diingat, pembengkakan dan memar pada tahap ini dapat mempengaruhi penilaian. \n\nHasil terbaik LAMS seringkali terlihat di bulan ketiga.",
-);
--- lib/features/faq/presentation/pages/faq_page.dart
+++ lib/features/faq/presentation/pages/faq_page.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/routes/app_route.dart';
 import '../../../../core/ui/extensions/build_context_extension.dart';
 import '../../../../core/ui/extensions/theme_data_extension.dart';
 import '../../../../core/ui/widget/images/network_image_widget.dart';
-import '../../data/models/faq_model.dart';
+import '../../../../core/utils/service_locator.dart';
+import '../../domain/entities/faq_entity.dart';
+import '../cubit/faqs/faqs_cubit.dart';
 import 'faq_details_page.dart';
 
-class FaqPage extends StatefulWidget {
+class FaqPage extends StatelessWidget {
   const FaqPage({super.key});
 
   @override
-  State<FaqPage> createState() => _FaqPageState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => sl<FaqsCubit>(),
+      child: const _Body(),
+    );
+  }
 }
 
-class _FaqPageState extends State<FaqPage> {
+class _Body extends StatefulWidget {
+  const _Body();
+
+  @override
+  State<_Body> createState() => __BodyState();
+}
+
+class __BodyState extends State<_Body> {
+  @override
+  void initState() {
+    super.initState();
+
+    _init();
+  }
+
+  void _init() {
+    // get faqs
+    _onGetFaqs();
+  }
+
+  Future<void> _onGetFaqs() async {
+    await BlocProvider.of<FaqsCubit>(context).getFaqs();
+  }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = context.theme.appTextTheme;
@@ -53,58 +84,67 @@ class _FaqPageState extends State<FaqPage> {
               padding: EdgeInsets.symmetric(
                 horizontal: context.paddingHorizontal,
               ),
-              child: Wrap(
-                spacing: 16,
-                runSpacing: 16,
-                children: mockFaqs.map(
-                  (faq) {
-                    return GestureDetector(
-                      onTap: () => _onFaqDetails(
-                        faq: faq,
-                      ),
-                      child: Container(
-                        width: context.width / 2.4,
-                        height: context.width / 2.4,
-                        decoration: BoxDecoration(
-                          color: colorScheme.primary,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: NetworkImageWidget(
-                                fit: BoxFit.cover,
-                                imageUrl: faq.imageUrl,
-                                borderRadius: const BorderRadius.vertical(
-                                  top: Radius.circular(20),
-                                ),
+              child: BlocBuilder<FaqsCubit, FaqsState>(
+                builder: (context, state) {
+                  if (state is FaqsLoaded) {
+                    return Wrap(
+                      spacing: 16,
+                      runSpacing: 16,
+                      children: state.faqs.map(
+                        (faq) {
+                          return GestureDetector(
+                            onTap: () => _onFaqDetails(
+                              faq: faq,
+                            ),
+                            child: Container(
+                              width: context.width / 2.4,
+                              height: context.width / 2.4,
+                              decoration: BoxDecoration(
+                                color: colorScheme.primary,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: NetworkImageWidget(
+                                      fit: BoxFit.cover,
+                                      imageUrl: faq.imageUrl,
+                                      borderRadius: const BorderRadius.vertical(
+                                        top: Radius.circular(20),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 16,
+                                    ),
+                                    child: Text(
+                                      faq.title ?? '',
+                                      maxLines: 3,
+                                      textAlign: TextAlign.center,
+                                      style: textTheme.labelMedium.copyWith(
+                                        color:
+                                            colorScheme.surfaceContainerBright,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                ],
                               ),
                             ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 16,
-                              ),
-                              child: Text(
-                                faq.title ?? '',
-                                maxLines: 3,
-                                textAlign: TextAlign.center,
-                                style: textTheme.labelMedium.copyWith(
-                                  color: colorScheme.surfaceContainerBright,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                          ],
-                        ),
-                      ),
+                          );
+                        },
+                      ).toList(),
                     );
-                  },
-                ).toList(),
+                  }
+
+                  return const SizedBox.shrink();
+                },
               ),
             ),
             SizedBox(
@@ -117,7 +157,7 @@ class _FaqPageState extends State<FaqPage> {
   }
 
   void _onFaqDetails({
-    required FaqModel faq,
+    required FaqEntity faq,
   }) {
     // go to faq details page
     context.goNamed(
