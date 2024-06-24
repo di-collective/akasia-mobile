fa3f3fff77dee0f81d753e10f0211c0672dffa1e
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2024-06-24 11:38:14 +0700

    fix: Faq config

--- lib/features/faq/data/datasources/faq_config.dart
+++ lib/features/faq/data/datasources/faq_config.dart
@@ -13,56 +13,92 @@ class FaqConfig {
         },
         {
           "title": "Mengapa salah satu sisi ukuran dan bentuknya berbeda?",
+          "description":
+              "Jelaskan kepada pelanggan bahwa peredaran darah meningkat lebih cepat di sisi yang biasanya lebih sering digunakan, sehingga mempercepat pemulihan. Pada saat ini, perbedaan memar dan pembengkakan mungkin menciptakan ilusi ukuran yang tidak rata di kedua sisi. Berikan panduan tentang bagaimana hal-hal mungkin terlihat berbeda seiring waktu."
         },
         {
           "title":
               "Kapan perasaan kesemutan akan hilang? (beberapa orang mungkin masih merasakan kesemutan setelah 3 minggu)",
+          "description":
+              "Pelanggan mungkin mengalami sensasi yang tidak biasa, seperti rasa sakit atau kekakuan, ketika disentuh, yang tidak terasa seperti bagian tubuh atau daging mereka sendiri. Sensasi abnormal ini dapat berlangsung sekitar sebulan dan mungkin menunjukkan regenerasi saraf sensorik—bagian alami dari proses pemulihan. Pastikan untuk memberi mereka jaminan bahwa sensasi tersebut diharapkan kembali sepenuhnya. Jika sensasi kesemutan tetap atau menjadi intens dan tidak nyaman bahkan setelah sebulan, kita dapat mengatur konsultasi dengan Direktur [Nama direktur].",
         },
         {
           "title":
               "Selain krim untuk memar, apakah ada cara lain untuk segara menghilangkan memar?",
+          "description":
+              "Melibatkan diri dalam aktivitas yang meningkatkan peredaran darah bermanfaat untuk pemulihan, termasuk peregangan, pijatan lembut selama mandi, dan berendam kaki. Meyandarkan kaki ke dinding dalam bentuk huruf L juga dapat membantu.",
         },
         {
           "title":
               "Boleh munggunakan kompres air hangat/panas (kompress panas)? Jika tidak, kapan saya bisa melakukannya?",
+          "description":
+              "Pengompresan hot pack tidak disarankan, karena mengekspos tubuh pada suhu tinggi atau rendah ketika saraf dan sensasi masih tumpul dapat menyebabkan luka bakar dan kemerahan.",
         },
         {
           "title": "Apakah ada diet khusus/batasan khusus untuk diet?",
+          "description":
+              "Tidak ada batasan diet yang spesifik. Namun, untuk pemulihan yang optimal, disarankan untuk menghindari alkohol setidaknya selama sebulan sejak pasien mulai mengonsumsi antibiotik yang diresepkan. Alih-alih fokus pada jenis makanan tertentu, mengadopsi kebiasaan tidak makan berlebihan dapat bermanfaat untuk diet pasien.",
         },
         {
           "title": "Kapan saya boleh berolahraga kembali?",
+          "description":
+              "Pasien dapat berolahraga keesokan harinya setelah prosedur LAMS, tergantung pada kondisi pasien. Namun, mungkin sulit jika pasien merasakan ketidaknyamanan. Hindari latihan/olahraga yang terlalu berat dan fokus lebih pada peregangan ringan dan latihan aerobik yang dapat merangsang peredaran darah.",
         },
         {
           "title": "Kapan saya bisa mengenakan korset/compression garments?",
+          "description":
+              "LAMS adalah prosedur yang tidak memerlukan penggunaan compression garments/korset. Tidak apa-apa jika pasien tidak mengenakannya.",
         },
-        {"title": "Kapan saya bisa melakukan LAMS kembali?"},
-        {"title": "Haruskah saya melakukan after-care setiap 5 hari sekali."},
+        {
+          "title": "Kapan saya bisa melakukan LAMS kembali?",
+          "description":
+              "Berencana untuk melakukan LAMS lagi? Untuk melakukan perawatan LAMS di area yang sama, disarankan untuk menunggu setidaknya 6 bulan hingga 1 tahun. Waktu yang tepat dapat ditentukan melalui konsultasi lebih lanjut dengan direktur.",
+        },
+        {
+          "title": "Haruskah saya melakukan after-care setiap 5 hari sekali.",
+          "description":
+              "Kecuali untuk kasus khusus (seperti kesulitan jadwal), kami umumnya merekomendasikan sesi tindak lanjut dengan jarak 6 hingga 10 hari. Sesi yang konsisten sekali seminggu hingga waktu emas 3 bulan lebih efektif daripada sesi singkat dan sering, dan kami merekomendasikan ini untuk hasil optimal.",
+        },
         {
           "title":
               "Apakah saya bisa melakukan Mesotherapy dan Shockwave Therapy pada hari yang sama?",
+          "description":
+              "Jika pasien ingin melakukan perawatan tambahan, kami dapat menyediakannya (harap dicatat bahwa biaya tambahan mungkin berlaku). Dalam banyak kasus, klien memilih paket yang mencakup terapi kejut gelombang eksternal dan injeksi mesoterapi secara bersamaan, dan mereka konsisten menjalani perawatan ini selama sekitar 2 hingga 3 bulan setelah menyelesaikan perawatan pasca stpasienr.",
         },
         {
           "title":
               "Apakah ada cara untuk mendapatkan hasil yang lebih cepat atau untuk mengurangi ukuran di area tubuh terntentu?",
+          "description":
+              "Pasien dapat mendapatkan manfaat yang lebih besar melalui diet dan perawatan pascaperawatan yang konsisten untuk mengurangi volume lemak yang tersisa.",
         },
         {
           "title":
               "Jenis obat/injeksi apa yang disuntikkan ke dalam tubuh saya?",
+          "description":
+              "Kami menggunakan obat yang dapat mempromosikan dan merangsang peredaran darah yang mengakibatkan pemecahan dan penghilangan lemak.",
         },
         {
           "title":
               "Setelah menerima Mesotherapy, seluruh tubuh saya gatal dan menjadi merah. Apakah saya alergi terhadap serum? Bisakah serumnya diganti?",
+          "description":
+              "Sepertinya pasein mungkin mengalami reaksi alergi terhadap obat. Dalam kasus seperti ini,, kita sebaiknya merekomendasikan untuk mempertemukan pasien dengan direktur rumah sakit untuk penilaian lebih lanjut. Jika ternyata alerginya cukup berat atau parah, direktur rumah sakit dapat menyarankan untuk menghentikan mesoterapi dan menyarankan prosedur alternatif (misalnya, RF).",
         },
         {
           "title": "Hingga kapan sebaiknya saya mengonsumsi pil diet?",
+          "description":
+              "Mengonsumsi pil diet selama proeses menjaga berata badan adalah hal yang baik. Jika pasien mengalami ketidaknyamanan, beri tahu rumah sakit selama kunjungan mereka selanjutnya untuk konsultasi lebih lanjut.",
         },
         {
           "title":
               "Apakah obat yang digunakan selama prosedur LAMS adalah obat untuk menghancurkan lemak?",
+          "description":
+              "Pil ini berperan dalam mengekspansi dan memecah lemak.",
         },
         {
           "title":
               "Jika berat badan saya sekitar 98kg, apakah saya bisa meliha hasil dengan hanya satu kali LAMS? Dan berapa lama waktu yang dibutuhkan untuk perawatannya?",
+          "description":
+              "Daripada fokus pada angka yang terlalu spesifik, pasien dapat fokus pada diet untuk mengurangi volume lemak yang tersisa dan berpartisipasi aktif dalam perawatan pascaperawatan (after-care) yang konsisten untuk lebih memecah dan menghilangkan lemak, pasien dapat mencapai hasil dan kepuasan yang diinginkan dengan lebih cepat. Umumnya, waktu terbiak (golden time) untuk LAMS adalah sekitar/ setelah 3 bulan. Terus menjaga tubuh melalui stimulasi prosedural selama setidaknya 3 bulan akan memaksimalkan efek.",
         }
       ],
     },
