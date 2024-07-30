05960e92c95bddec836cfdbf81fba441bc8948a7
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2024-07-30 14:16:40 +0700

    feat: all nutrition data

--- lib/features/health/presentation/pages/all_nutrition_data_page.dart
+++ lib/features/health/presentation/pages/all_nutrition_data_page.dart
@@ -4,8 +4,14 @@ import 'package:go_router/go_router.dart';
 
 import '../../../../core/routes/app_route.dart';
 import '../../../../core/ui/extensions/build_context_extension.dart';
+import '../../../../core/ui/extensions/date_time_extension.dart';
+import '../../../../core/ui/extensions/object_extension.dart';
 import '../../../../core/ui/extensions/theme_data_extension.dart';
+import '../../../../core/ui/widget/states/state_empty_widget.dart';
+import '../../../../core/ui/widget/states/state_error_widget.dart';
+import '../../domain/entities/nutrition_activity_entity.dart';
 import '../cubit/nutrition/nutrition_cubit.dart';
+import '../widgets/activity_item_widget.dart';
 import '../widgets/actvity_item_loading_widget.dart';
 import 'sleep_details_page.dart';
 
@@ -45,103 +51,80 @@ class _AllNutritionDataPageState extends State<AllNutritionDataPage> {
       backgroundColor: colorScheme.surfaceBright,
       body: BlocBuilder<NutritionCubit, NutritionState>(
         builder: (context, state) {
-          // if (state is NutritionLoaded) {
-          // final nutritions = state.nutritions?.data;
-          // if (nutritions == null || nutritions.isEmpty) {
-          //   return const StateEmptyWidget();
-          // }
+          if (state is NutritionLoaded) {
+            final nutritions = state.nutritions?.data;
+            if (nutritions == null || nutritions.isEmpty) {
+              return const StateEmptyWidget();
+            }
 
-          // Map<DateTime, List<SleepActivityEntity>> sortedSleeps = {};
-          // for (int i = nutritions.length - 1; i > 0; i--) {
-          //   final sleep = nutritions[i];
+            Map<DateTime, List<NutritionActivityEntity>> sortedNutritions = {};
+            for (int i = nutritions.length - 1; i > 0; i--) {
+              final sleep = nutritions[i];
 
-          //   final fromDate = sleep.fromDate;
-          //   if (fromDate == null) {
-          //     continue;
-          //   }
+              final fromDate = sleep.fromDate;
+              if (fromDate == null) {
+                continue;
+              }
 
-          //   final data = nutritions.where((element) {
-          //     return element.fromDate?.isSameDay(other: fromDate) ?? false;
-          //   });
+              final data = nutritions.where((element) {
+                return element.fromDate?.isSameDay(other: fromDate) ?? false;
+              });
 
-          //   sortedSleeps.addAll({
-          //     fromDate.firstHourOfDay: data.toList(),
-          //   });
-          // }
+              sortedNutritions.addAll({
+                fromDate.firstHourOfDay: data.toList(),
+              });
+            }
 
-          //   return ListView.separated(
-          //     itemCount: sortedSleeps.keys.length,
-          //     primary: false,
-          //     shrinkWrap: true,
-          //     padding: EdgeInsets.symmetric(
-          //       horizontal: context.paddingHorizontal,
-          //       vertical: 16,
-          //     ),
-          //     separatorBuilder: (context, index) {
-          //       return const Divider();
-          //     },
-          //     itemBuilder: (context, index) {
-          //       final sleep = sortedSleeps.entries.toList()[index];
+            return ListView.separated(
+              itemCount: sortedNutritions.keys.length,
+              primary: false,
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(
+                horizontal: context.paddingHorizontal,
+                vertical: 16,
+              ),
+              separatorBuilder: (context, index) {
+                return const Divider();
+              },
+              itemBuilder: (context, index) {
+                final nutritions = sortedNutritions.entries.toList()[index];
 
-          //       // date range
-          //       DateTime? fromDate;
-          //       DateTime? toDate;
-          //       String formattedDateRange = '';
-          //       Duration totalDuration = const Duration();
+                final date = nutritions.key.formatDate(
+                  format: 'dd MMM',
+                );
 
-          //       for (final item in sleep.value) {
-          //         final currentFromDate = item.fromDate;
-          //         final currentToDate = item.toDate;
-          //         if (currentFromDate == null || currentToDate == null) {
-          //           continue;
-          //         }
+                double totalNutritionsInDay = 0;
+                for (final item in nutritions.value) {
+                  final value = item.value;
+                  if (value == null) {
+                    continue;
+                  }
 
-          //         if (fromDate == null || currentFromDate.isBefore(fromDate)) {
-          //           fromDate = currentFromDate;
-          //         }
+                  totalNutritionsInDay += value;
+                }
 
-          //         if (toDate == null || currentToDate.isAfter(toDate)) {
-          //           toDate = currentToDate;
-          //         }
-
-          //         final duration = currentToDate.difference(currentFromDate);
-
-          //         totalDuration += duration;
-          //       }
-
-          //       final formattedTotalDuration =
-          //           "${totalDuration.inHours}hrs ${totalDuration.remainingMinutes}min";
-
-          //       if (fromDate != null && toDate != null) {
-          //         formattedDateRange = fromDate.formatDateRange(
-          //           endDate: toDate,
-          //           formatEndDate: 'dd MMM',
-          //         );
-          //       }
-
-          //       return ActivityItemWidget(
-          //         isFirst: index == 0,
-          //         isLast: index == sortedSleeps.keys.length - 1,
-          //         title: formattedDateRange,
-          //         value:
-          //             "${sleep.value.length} interval ($formattedTotalDuration)",
-          //         onTap: () {
-          //           _onNutrition(
-          //             params: SleepDetailsPageParams(
-          //               sleeps: sleep.value,
-          //               formattedDateRange: formattedDateRange,
-          //               totalDuration: totalDuration,
-          //             ),
-          //           );
-          //         },
-          //       );
-          //     },
-          //   );
-          // } else if (state is NutritionError) {
-          //   return StateErrorWidget(
-          //     description: state.error.message(context),
-          //   );
-          // }
+                return ActivityItemWidget(
+                  isFirst: index == 0,
+                  isLast: index == sortedNutritions.keys.length - 1,
+                  title: date ?? "",
+                  value: "${totalNutritionsInDay}cal",
+                  onTap: () {
+                    // _onNutrition(
+                    //   params: SleepDetailsPageParams(
+                    //     sleeps: sleep.value,
+                    //     formattedDateRange: formattedDateRange,
+                    //     totalDuration: totalDuration,
+                    //   ),
+                    // );
+                  },
+                );
+              },
+            );
+          } else if (state is NutritionError) {
+            return StateErrorWidget(
+              description: state.error.message(context),
+            );
+          }
 
           return ListView.builder(
             itemCount: 10,
